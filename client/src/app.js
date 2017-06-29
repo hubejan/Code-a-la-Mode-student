f318f8074d9f2b4a385cb5eaf0e602c5a22a8d04
Hubert J <hubejan@gmail.com>
2017-06-29 15:13:01 -0400

    Another set of tries

--- client/src/app.js
+++ client/src/app.js
@@ -3,8 +3,19 @@ import MainView from './components/MainView';
 import Main from './components/Main';
 import Button from './components/animatedButton';
 
+const initializeFS = () => {
+  console.log('Initializing FS...')
+  var html5fs = new BrowserFS.FileSystem.HTML5FS(10, window.TEMPORARY);
+  html5fs.allocate();
+  BrowserFS.initialize(html5fs);
+  console.dir(html5fs);
+  console.log('FS Initialized!!')
+}
+
+
 const App = () => (
     <div>
+      {initializeFS()}
       <Main />
     </div>
 );
--- client/src/components/MainView.js
+++ client/src/components/MainView.js
@@ -5,44 +5,80 @@ import { connect } from 'react-redux';
 import Navbar from './Navbar';
 import store from '../store';
 import FileTree from 'react-filetree-electron';
+const BrowserFS = require('browserfs');
+const Promise = require('bluebird');
 //  import fs from 'download-npm'
 // import fs from 'fs-es6';
 // import fs from 'bro-fs';
 //import fs from ''
 
-const logSuccess = (value) => console.log(`Success with: ${value} !!!`)
-
-const makeFilesTree = (fileSystem) => {
-  for (let i = 0; i <= 10; i++){
-      fileSystem.writeFile(`file${i}.txt`, `File ${i} content !!!`)
-      .then(logSuccess);
-  }
-}
+var fs = Promise.promisifyAll(require('fs'));
 
 
-const createFileSystem  = () => {
-var lsfs = new BrowserFS.FileSystem.LocalStorage();
-  // Initialize it as the root file system.
-  BrowserFS.initialize(lsfs);
+// var fs = require('fs');
+// const logSuccess = (value) => console.log(`Success with: ${value} !!!`)
 
-  var fs = require('fs');
-//   fs.writeFileSync('/test22.txt', 'Cool, I can do this in the browser!', function(err) {
-//     console.log(`File created ? ${fs}`)
-//     console.dir(fs)
-//   fs.readFile('/test22.txt', function(err, contents) {
-//     // console.log(contents.toString());
-//     console.log(contents);
+// const makeFilesTree = (fileSystem) => {
+//   for (let i = 0; i <= 10; i++){
+//       fileSystem.writeFile(`file${i}.txt`, `File ${i} content !!!`)
+//       .then(logSuccess);
+//   }
+// }
+
+// const readF = (file) => {
+
+//   fs.readdirAsync('/',(err,rv)=>{console.log(rv);});
+//   // fs.readFile('/test22.txt', function(err, contents) {
+//   //   console.log(contents.toString());
+//   //   // console.log(contents);
+//   // });
+// }
+
+// const writeF = (file) => {
+//   fs.writeFile( file, 'Cool, I can do this in the browser!');
+// }
+
+// const initializeFS = () => {
+//   console.log('Initializing FS...')
+//   var html5fs = new BrowserFS.FileSystem.HTML5FS(10);
+//   html5fs.allocate();
+//   BrowserFS.initialize(html5fs);
+//   console.dir(html5fs);
+//   console.log('FS Initialized!!')
+// }
+
+
+
+// const createFileSystem  = () => {
+//   fs = require('fs');
+// //   fs.writeFileSync('/test22.txt', 'Cool, I can do this in the browser!', function(err) {
+// //     console.log(`File created ? ${fs}`)
+// //     console.dir(fs)
+// //   fs.readFile('/test22.txt', function(err, contents) {
+// //     // console.log(contents.toString());
+// //     console.log(contents);
+// //   });
+// // });
+// // fs.mkdirSync('/');
+// let fileName = '/test22.txt';
+
+// try {
+//   fs.writeFile( fileName, 'Cool, I can do this in the browser!');
+// }catch (err) {
+//   console.log(`Catch error: ${err}`)
+//   console.dir(err)
+// }
+// // ,(err)=>{
+// //     console.log('errorWWW',err);
+// // });
+
+//   fs.readFile(fileName, function(err, contents) {
+//     console.log(contents.toString());
+//     console.log(err);
 //   });
-// });
-// fs.mkdirSync('/');
-fs.writeFileSync('/test22.txt', 'Cool, I can do this in the browser!');
-fs.readFile('/test22.txt', function(err, contents) {
-    console.log(contents.toString());
-    // console.log(contents);
-  });
 
 
-console.log(fs.readdirSync('/'))
+// console.log(fs.readdirSync('/'))
 //  (ret) => {console.log(`Directory entries: ${ret}`)})
 
 
@@ -63,7 +99,7 @@ console.log(fs.readdirSync('/'))
 //   'file-loader@^0.11.2', // for example, express@4.0.0-rc4 or tape@latest etc
 //   '/tmp' // the path to download
 // ).then(() => {console.log('downloaded !!!')}, () => {console.log('ERROR with loading')} )
-};
+// };
 
 const loadCode = () => {
   return null;
@@ -74,9 +110,14 @@ const MainView = ({ code, snapshots, selectSnapshot }) => (
     <div className="row">
       <div className="col-md-12">
         <h1>NAVBAR</h1>    
-        <FileTree directory={'/'} />
+        <FileTree directory={'/'} fs={fs} />
       </div>
+      <button id="init" type="button" onClick={() => { initializeFS();}}> Init </button>
       <button id="load" type="button" onClick={() => { createFileSystem();}}> Load </button>
+      <button id="writeF" type="button" onClick={() => { writeF('/test22.txt');}}> writeF </button>
+      <button id="readF" type="button" onClick={() => { readF('/test22.txt');}}> readF </button>
+      <button id="show" type="button" onClick={() => { renderTree() }}> show </button>
+      
     </div>
     <div className="row">
       <div className="col-md-3">
