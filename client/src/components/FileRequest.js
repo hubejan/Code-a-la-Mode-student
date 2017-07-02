c656f37c42266e28689847227ba378af8449ac3b
Hubert J <hubejan@gmail.com>
2017-07-02 17:53:36 -0400

    Some material ui refactoring

--- client/src/components/FileRequest.js
+++ client/src/components/FileRequest.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Flexbox from 'flexbox-react';
 
 /* realistically probably want a min length on question? */
 const FileRequest = ({ handleChange, handleSubmit, inputValue }) => {
   return (
-    <div>
-
+    <Flexbox>
       <form onSubmit={handleSubmit}>
-        <input
+        <TextField
           type="text"
           onChange={handleChange}
           value={inputValue}
           placeholder="enter something like /folder/...filename.js"
         />
-        <button type="submit" disabled={inputValue.length === 0} >
+        <RaisedButton type="submit" disabled={inputValue.length === 0} >
           SEND
-        </button>
-      </form>
+        </RaisedButton>
+        <p>
           If you don't have /input.js in your ROOT directory, go into the client/src/containers/FileRequestContainer.js and change inputValue to be whatever file path you want to request from your own computer (like /home/hubert/test.js)
-    </div>
+        </p>
+      </form>
+    </Flexbox>
   );
 };
 
