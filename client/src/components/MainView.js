c41e0e8d38187d5d29e572cd28ae0f020f0cfc31
Hubert J <hubejan@gmail.com>
2017-07-02 14:13:38 -0400

    MaterialUi components in used.

--- client/src/components/MainView.js
+++ client/src/components/MainView.js
@@ -10,6 +10,10 @@ import Promise from 'bluebird';
 import fs from 'fs';
 import Flexbox from 'flexbox-react';
 import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import Drawer from 'material-ui/Drawer';
+import Paper from 'material-ui/Paper';
+
 
 const writeNum = (num) => {
   const test = `/OMG${folder}/test${num}`;
@@ -53,12 +57,19 @@ const logGet = (dir) => {
   });
 };
 
+const style = {
+  margin: 12,
+};
+
 class MainView extends Component {
   componentDidMount() {
     const { socket } = this.props;
+    this.state = { open: false };
     // can attach any additional listeners here
     // editor listener is currently inside CodeView, but can optionally be moved here
   }
+
+  handleToggle = () => this.setState({open: !this.state.open});
 
   render() {
     const { snapshots, selectSnapshot, socket } = this.props;
@@ -66,20 +77,28 @@ class MainView extends Component {
     <Flexbox display="flex" flexDirection="column" flexGrow={1} minWidth="100vw">
       <Flexbox flexDrirection="row">
         <AppBar title="Code-a-la-Mode" iconClassNameRight="muidocs-icon-navigation-expand-more"> 
-        <button id="write1" type="button" onClick={() => writeNum('1')} > write1 </button>
-        <button id="write2" type="button" onClick={() => writeNum('2')} > write2 </button>
-        <button id="read1" type="button" onClick={() => readNum('1')} > read1 </button>
-        <button id="read2" type="button" onClick={() => readNum('2')} > read2 </button>
-        <button id="read" type="button" onClick={() => readDir('/')} > dir </button>
+        <RaisedButton label="write1" style={style} onClick={() => writeNum('1')} />
+        <RaisedButton label="write2" style={style} onClick={() => writeNum('2')} />
+        <RaisedButton label="read1" style={style} onClick={() => readNum('1')} />
+        <RaisedButton label="read2" style={style} onClick={() => readNum('2')} />
+        <RaisedButton label="dir" style={style} onClick={() => readDir('/')} />
+        <RaisedButton label="Questions" style={style} onClick={() => handleToggle()} />
         </AppBar>
       </Flexbox>
       <Flexbox element="main" flexDirection="row">
         <Flexbox element="aside" flexGrow={3}>
+        <Paper style={style} zDepth={2} >
           <FileTree directory={'/'} />
+        </Paper>
         </Flexbox>
         <Flexbox flexGrow={3}>
+        <Paper style={style} zDepth={5} >
           <CodeView socket={socket} />
+        </Paper>
         </Flexbox>
+        <Drawer width={200} openSecondary={true} open={true} >
+          <AppBar title="Tickets" />
+        </Drawer>        
       </Flexbox>
     </Flexbox>
     )
